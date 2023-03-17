89c67d54928445ebe16291c95415bd7a17c1fd49
flybaruk <66071456+flybaruk@users.noreply.github.com>
2023-03-16 21:04:05 -0300

    Update func_cobrinha.js
    quando a cobrinha vira agora não é mais instantâneo, mas ta bugado, tenho que ajeitar

--- func_cobrinha.js
+++ func_cobrinha.js
@@ -4,8 +4,10 @@ const altura = canvas.height;
 const ctx = canvas.getContext("2d");
 //dimensão da cobrinha
 const aumenta_tamanho = 5;
-const comp_cobra = 5;
-const alt_cobra = 3;
+let comp_cobra = 5;
+let alt_cobra = 3;
+let pos_iniX = 10;
+let pos_iniY = 10;
 //velocidade é baseado pelo mov, o SemMov existe pois só é possivel movimentar no eixo x ou y
 const mov = 5;
 const SemMov = 0;
@@ -13,9 +15,13 @@ const SemMov = 0;
 const intervalID = setInterval(jogo, 50);
 
 class Cobrinha { 
-    constructor(x, y, com, alt){
-        this.x = x;
-        this.y = y;
+    constructor(com, alt){
+        this.body = [
+            {x:pos_iniX, y:pos_iniY},
+            {x:pos_iniX - 1, y:pos_iniY},
+            {x:pos_iniX - 2, y:pos_iniY},
+            {x:pos_iniX - 3, y:pos_iniY}
+        ];
         this.com = com;
         this.alt = alt;
         this.velx = 0;
@@ -23,9 +29,14 @@ class Cobrinha {
     }
 
     novaPosicao(){
-        this.x += this.velx;
-        this.y += this.vely;
+        for (let i = this.body.length - 1; i > 0; i--) {
+            this.body[i].x = this.body[i-1].x;
+            this.body[i].y = this.body[i-1].y;
+        }
+        this.body[0].x += this.velx;
+        this.body[0].y += this.vely;
     }
+
     
 }
 
@@ -39,7 +50,7 @@ class comida{
 }
 
 // É o que faz a cobrinha aparecer
-let minhaCobrinha = new Cobrinha(10, 10, comp_cobra, alt_cobra); 
+let minhaCobrinha = new Cobrinha(comp_cobra, alt_cobra); 
 ctx.fillStyle = "#FF0000"; 
 ctx.fillRect(minhaCobrinha.x, minhaCobrinha.y, minhaCobrinha.com, minhaCobrinha.alt);
 //faz a comida aparecer
@@ -49,32 +60,34 @@ ctx.fillRect(Comida.x, Comida.y, Comida.TamC, Comida.TamA);
 
 
 //Controle da cobrinha
-document.addEventListener('keydown', (event) => {
-    if (event.key === "ArrowDown" && minhaCobrinha.vely !== -mov) {
-        minhaCobrinha.velx = SemMov;
-        minhaCobrinha.vely = mov; 
-        minhaCobrinha.com = alt_cobra;
-        minhaCobrinha.alt = comp_cobra;
-    }
-    else if (event.key === "ArrowUp" && minhaCobrinha.vely !== mov){
-        minhaCobrinha.velx = SemMov;
-        minhaCobrinha.vely = -mov;
-        minhaCobrinha.com = alt_cobra;
-        minhaCobrinha.alt = comp_cobra;
-    }
-    else if (event.key === "ArrowRight" && minhaCobrinha.velx !== -mov){
-        minhaCobrinha.velx = mov;
-        minhaCobrinha.vely = SemMov;
-        minhaCobrinha.com = comp_cobra;
-        minhaCobrinha.alt = alt_cobra;
-    }
-    else if (event.key === "ArrowLeft" && minhaCobrinha.velx !== mov){
-        minhaCobrinha.velx = -mov;
-        minhaCobrinha.vely = SemMov;
-        minhaCobrinha.com = comp_cobra;
-        minhaCobrinha.alt = alt_cobra;
-    }
-})
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === "ArrowDown" && minhaCobrinha.vely !== -mov) {
+            minhaCobrinha.velx = SemMov;
+            minhaCobrinha.vely = mov; 
+            minhaCobrinha.com = alt_cobra;
+            minhaCobrinha.alt = comp_cobra;
+        }
+        else if (event.key === "ArrowUp" && minhaCobrinha.vely !== mov){
+            minhaCobrinha.velx = SemMov;
+            minhaCobrinha.vely = -mov;
+            minhaCobrinha.com = alt_cobra;
+            minhaCobrinha.alt = comp_cobra;
+        }
+        else if (event.key === "ArrowRight" && minhaCobrinha.velx !== -mov){
+            minhaCobrinha.velx = mov;
+            minhaCobrinha.vely = SemMov;
+            minhaCobrinha.com = comp_cobra;
+            minhaCobrinha.alt = alt_cobra;
+        }
+        else if (event.key === "ArrowLeft" && minhaCobrinha.velx !== mov){
+            minhaCobrinha.velx = -mov;
+            minhaCobrinha.vely = SemMov;
+            minhaCobrinha.com = comp_cobra;
+            minhaCobrinha.alt = alt_cobra;
+        }
+    })
+
 
 //função para atualizar as imagens que aparecem na tela
 function jogo(){
@@ -82,7 +95,9 @@ function jogo(){
     ctx.clearRect(0, 0, comprimento, altura);
     minhaCobrinha.novaPosicao();
     ctx.fillStyle = "#FF0000";
-    ctx.fillRect(minhaCobrinha.x, minhaCobrinha.y, minhaCobrinha.com, minhaCobrinha.alt); 
+    for (let i = 0; i < minhaCobrinha.body.length; i++) {
+        ctx.fillRect(minhaCobrinha.body[i].x, minhaCobrinha.body[i].y, minhaCobrinha.com, minhaCobrinha.alt);
+    }
     //ficar repetindo comida
     let Comida = new comida(100, 100, 3, 3);
     ctx.fillStyle = "#0000FF";
@@ -107,9 +122,11 @@ function aumentar_cobrinha(){
     if (Comida.x === minhaCobrinha.x && Comida.y === minhaCobrinha.y) {
         if (minhaCobrinha.alt > minhaCobrinha.com) {
             minhaCobrinha.alt += aumenta_tamanho;
+            comp_cobra += aumenta_tamanho;
         }
         else{
             minhaCobrinha.com += aumenta_tamanho;
+            comp_cobra += aumenta_tamanho;
         }
     }
 }
